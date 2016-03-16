94b5c66fb4b47293263bf0086f18da7aab9b73ca
Justin Davis <jjustind@gmail.com>
2016-03-16 08:47:11 -0500

    updated

--- AllMillsViewController.m
+++ AllMillsViewController.m
@@ -94,7 +94,7 @@
     searchView = [[CanonMediaMillSearchOverlay alloc] init];
     searchView.delegate = self;
     
-    // Header build methods - Don't mix up the order.
+    // Header build methods 
     [self buildHeaderView];
     [self buildTableView];
     
